68a0ad74d65ff96265c07d9f8b8bba4aefbfac95
adgang <adgang@tuta.io>
2024-10-19 03:05:04 +0530

    fixed issue with ESM modules not handled by vite

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,12 @@
 // app.config.ts
+import { pluginDeno } from "@deno-plc/vite-plugin-deno";
+
 import { defineConfig } from "npm:@tanstack/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+    plugins: [
+        pluginDeno({
+            // see configuration docs
+        }),
+    ],
+});
